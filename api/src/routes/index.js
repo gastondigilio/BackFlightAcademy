4e61092d3433d33c57d71b3cfa8d09f8dcf70763
gastondigilio <gastondigilio@gmail.com>
2022-01-13 16:06:34 -0300

    nextHoursRoutes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const usersRoutes = require('./usersRoutes');
 const examsRoutes = require('./examsRoutes');
 const hoursRoutes = require('./hoursRoutes');
+const nextHoursRoutes = require('./nextHoursRoutes');
 const router = Router();
 
 router.use(express.urlencoded({ extended: true, limit: "50mb" }));
@@ -10,6 +11,7 @@ router.use(express.json({ limit: "50mb" }));
 
 router.use('/users', usersRoutes);
 router.use('/exams', examsRoutes);
-router.use('/hours', hoursRoutes)
+router.use('/hours', hoursRoutes);
+router.use('/nextHours', nextHoursRoutes);
 
 module.exports = router;
