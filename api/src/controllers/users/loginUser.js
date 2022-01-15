a9405cc7741f56b533031adb887ca358c3e12e08
enzo388 <enzo.vazquez.388@gmail.com>
2022-01-14 22:10:15 -0300

    login rutes fix

--- api/src/controllers/users/loginUser.js
+++ api/src/controllers/users/loginUser.js
@@ -1,30 +1,33 @@
-const { json } = require('body-parser');
+
 const {Users} = require ('../../db.js');
 
 async function loginUser(req, res, next){
-    const {userId, pass, email} = req.body ;
+    const {pass, email} = req.body ;
     try {
-        if(!userId){
+        if(!email){
             
-            return res.status(200).json({mensaje: "userId not Found"})
-        }
-        if(pass &&  email) {
-            const user = await Users.findOne({ where: { id: userId} })
-                if(user){
-                     if(user.pass === pass && user.email === email){
-                        return res.status(200).json({message: "Las credenciales coinciden"});
+            return res.status(400).json({message: "email not Found"})
+        }else {
+            if(pass &&  email) {
+                const user = await Users.findOne({ where: { email: email} })
+                    if(user){
+                         if(user.pass === pass && user.email === email){
+                             console.log(user.pass, pass, user.email, email)
+                            return res.status(200).json(user);
+                        }else{
+                            return res.status(400).json({message: "Las credenciales  no coinciden"});
+                        } 
+                        
+                        
                     }else{
-                        return res.status(400).json({message: "Las credenciales  no coinciden"});
-                    } 
-                    
-                    
-                }else{
-                    return res.status(400).json({message: 'users not found'});
-                }
-        }else{
-            return res.status(400).json({mensaje : " pass o email no funciona"})
+                        return res.status(400).json({message: 'users not found'});
+                    }
+            }else{
+                return res.status(400).json({message : " pass o email no funciona"})
+            }
+          
         }
-      
+       
       
     } catch (error) {
         next(error);
