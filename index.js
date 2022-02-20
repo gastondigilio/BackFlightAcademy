da4f725fbbf0f1f0f65ef2d5b1989c2f46af6217
gastondigilio <gastondigilio@gmail.com>
2022-02-20 13:46:11 -0300

    sarasa

--- index.js
+++ index.js
@@ -55,7 +55,8 @@ const createDB = async () => {
 
 // Syncing all the models at once.
 
-conn.sync({ force: false }).then(() => {
+
+conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT || 3001, async () => {
     console.log("Espere a que se cree la base de datos..");
     console.time("Se creo la base de datos con exito");
