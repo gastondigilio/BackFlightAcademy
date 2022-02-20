3e0753c4473ff62d05180b74251cf668ecde7f66
MaarGoesGil <abrilmartinagil@gmail.com>
2022-02-20 14:13:24 -0300

    carga de hroas instructor o admin

--- src/routes/hoursRoutes.js
+++ src/routes/hoursRoutes.js
@@ -3,12 +3,12 @@ const getHoursByIdUser = require('../controllers/hours/getHoursByIdUser')
 const uploadHours = require('../controllers/hours/uploadHours');
 const updateHours = require('../controllers/hours/updateHours')
 const { verifyTokenAndTransformToId } = require('../middlewares/tokens');
-const onlyAdminOrBelongingToTheUser = require('../middlewares/onlyAdminOrBelongingToTheUser');
+const verifyAdminOrInstruct = require('../middlewares/verifyAdminOrInstruct');
 
 const router = Router();
 
 router.get('/', verifyTokenAndTransformToId, getHoursByIdUser);
-router.post('/upload', verifyTokenAndTransformToId, onlyAdminOrBelongingToTheUser, uploadHours);
-router.patch('/update', verifyTokenAndTransformToId, onlyAdminOrBelongingToTheUser, updateHours);
+router.post('/upload', verifyTokenAndTransformToId, verifyAdminOrInstruct, uploadHours);
+router.patch('/update', verifyTokenAndTransformToId, verifyAdminOrInstruct, updateHours);
 
 module.exports = router;
